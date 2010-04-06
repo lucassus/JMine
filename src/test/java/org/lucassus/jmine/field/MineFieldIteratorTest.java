2dcdfd090572f0b3af76523b264c630232113d14
Łukasz Bandzarewicz <lucassus@gmail.com>
2010-04-06 19:57:09 +0200

    Test for the Field Iterator.

---
+++ src/test/java/org/lucassus/jmine/field/MineFieldIteratorTest.java
@@ -0,0 +1,46 @@
+package org.lucassus.jmine.field;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MineFieldIteratorTest {
+
+    private Field[][] fields;
+    private Field field1, field2, field3, field4;
+    private MineFieldIterator instance;
+
+    @Before
+    public void setUp() {
+        field1 = new Field();
+        field2 = new Field();
+        field3 = new Field();
+        field4 = new Field();
+
+        fields = new Field[][]{
+                    {field1, field2},
+                    {field3, field4}
+                };
+        instance = new MineFieldIterator(fields);
+    }
+
+    /**
+     * Test of next method, of class MineFieldIterator.
+     */
+    @Test
+    public void testHasNextAndNext() {
+        assertTrue(instance.hasNext());
+        assertEquals(field1, instance.next());
+
+        assertTrue(instance.hasNext());
+        assertEquals(field2, instance.next());
+
+        assertTrue(instance.hasNext());
+        assertEquals(field3, instance.next());
+
+        assertTrue(instance.hasNext());
+        assertEquals(field4, instance.next());
+
+        assertFalse(instance.hasNext());
+    }
+}
